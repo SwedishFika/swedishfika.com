2e577d5ab62e66a70b1300ddef4791a8f6248bd2
Johan Benjaminsson <johanb@karnhuset.net>
2014-02-16 14:22:00 +0100

    HTML5 > XHTML
    Among other adjustments.

--- header.php
+++ header.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html 
-     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html class="no-js">
 <head>
 	<title><?php wp_title(' | ', true, 'right'); ?><?php bloginfo('name'); ?></title>
-  	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-  	<meta name="author" content="Alexander Radsby &amp; Simon Kjellberg" />
-  	<meta name="language" content="EN" /> 
-  	<meta name="copyright" content="Copyright 2008" /> 
-  	<meta name="rating" content="general" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     
- 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" charset="utf-8" />
+ 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/vendor/normalize.css">
+ 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/style.css">
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
