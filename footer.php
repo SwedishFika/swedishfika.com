6d60853f5eb55b7caee7aee0f562333c630f5af7
Johan Benjaminsson <johanb@karnhuset.net>
2014-02-16 14:21:12 +0100

    Updated google tracking code

--- footer.php
+++ footer.php
@@ -14,14 +14,13 @@
 </div>
 <div id="bottom"></div>
 </div>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-12170777-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+	<script>
+		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		e.src='//www.google-analytics.com/analytics.js';
+		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+		ga('create','UA-12170777-1');ga('send','pageview');
+	</script>
 </body>
 </html>
