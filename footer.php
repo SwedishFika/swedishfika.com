e676a5a67ffade0e3a587e34eb1082d7431d2c0b
Ida Franceen <Ida.Franceen@creuna.se>
2014-02-23 21:42:37 +0100

    Changed width and footer

--- footer.php
+++ footer.php
@@ -1,17 +1,18 @@
-<div id="footer">
-		<div id="footer-left">
-			<p class="copyright">Copyright &copy; <?php echo date('Y'); ?>, All Rights Reserved</p>
-			<p>Design, images &amp; stylesheet may not be reused without permission.</p>
-			<p>This site has been designed. coded, browser tested and written by Alexander Radsby, Johan Benjaminsson, Ida Franceen &amp; Simon Kjellberg.</p>
-			<p>Crafted with <a href="http://validator.w3.org/check/referer" title="Validates this page">HTML</a>
-			&amp; <a href="http://jigsaw.w3.org/css-validator/validator?uri=http://swedishfika.com/wp-content/themes/swedishfika/style.css" title="Validates the CSS of this page">CSS</a>.</p>
-		</div>
-		<div id="footer-right">
-			<p><a href="/">Home</a> / <a href="/about">About</a> / <a href="/archives">Archives</a> / <a href="/contact">Contact</a></p>
-			<p><a href="/feed">RSS Feed</a> &amp; <a href="/feed/atom">Atom Feed</a></p>
-		</div>
 </div>
-<div id="bottom"></div>
+<div class="bottom">
+	<div class="footer">
+			<footer id="footer-left" role="content-info">
+				<p class="copyright">Copyright &copy; <?php echo date('Y'); ?>, All Rights Reserved</p>
+				<p>Design, images &amp; stylesheet may not be reused without permission.</p>
+				<p>This site has been designed. coded, browser tested and written by Alexander Radsby, Johan Benjaminsson, Ida Franceen &amp; Simon Kjellberg.</p>
+				<p>Crafted with <a href="http://validator.w3.org/check/referer" title="Validates this page">HTML</a>
+				&amp; <a href="http://jigsaw.w3.org/css-validator/validator?uri=http://swedishfika.com/wp-content/themes/swedishfika/style.css" title="Validates the CSS of this page">CSS</a>.</p>
+			</footer>
+			<div id="footer-right">
+				<p><a href="/">Home</a> / <a href="/about">About</a> / <a href="/archives">Archives</a> / <a href="/contact">Contact</a></p>
+				<p><a href="/feed">RSS Feed</a> &amp; <a href="/feed/atom">Atom Feed</a></p>
+			</div>
+	</div>
 </div>
 	<script>
 		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
