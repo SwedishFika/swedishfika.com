142e10810846c37023f5cba24a6b87070ae1fc10
Ida Franceen <Ida.Franceen@creuna.se>
2014-02-24 00:36:02 +0100

    Quick adjustment to single-page. Added a template-stylesheet for it as well.

--- single.php
+++ single.php
@@ -3,13 +3,12 @@ get_header();
 ?>
 <main>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="main-entry page">
+		<div class="single">
+            <h1><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+            <div class="pub-date">
+                <time><?php the_date(); ?></time>
+            </div>
 			<img src="<?php $value = get_post_custom_values("big"); echo $value[0];  ?>" class="big-image" alt=""  />
-			<h1><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-			<div class="datebox">
-				<div class="date"><?php the_date(); ?></div>
-				<!--<div class="month">DEC</div>-->
-			</div>
 			<div class='main-content'>
 				<?php the_content(__('Continue reading...')); ?>
 			</div>
