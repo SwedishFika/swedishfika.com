579c9bad76c13a9ed800e514dbfb7f97d483f460
Ida Franceen <Ida.Franceen@creuna.se>
2014-02-23 15:12:03 +0100

    Added article to listing of posts and left-aligned image. Looking better I think. At least right now.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ get_header();
 	
 	<?php //if (is_front_page()) {query_posts('showposts=3');}?>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="main-entry">
+		<article class="main-entry">
 			<a href="<?php the_permalink() ?>" rel="bookmark" class="big-image-link">
 			<img src="<?php $value = get_post_custom_values("big"); echo $value[0];  ?>" class="big-image" alt=""  />
 			</a>
@@ -17,7 +17,7 @@ get_header();
 			<div class='main-content'>
 				<?php the_content(__('Continue reading...')); ?>
 			</div>
-		</div>
+		</article>
     <?php endwhile; ?>
 		<?php if (show_posts_nav()) : ?>
 		<div class="navigation">
